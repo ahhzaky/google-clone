aa166821029b72ad9c375f95e3ae016561e37452
ahhzaky <siahmadzaky@gmail.com>
2020-09-19 23:02:34 +0700

    Now Backend

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import { Link } from "react-router-dom";
 import AppsIcon from "@material-ui/icons/Apps";
 import { Avatar } from "@material-ui/core";
+import Search from "./Search";
 
 function Home() {
   return (
@@ -24,6 +25,9 @@ function Home() {
           src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           alt=""
         />
+        <div className="home__inputContainer">
+          <Search />
+        </div>
       </div>
     </div>
   );
