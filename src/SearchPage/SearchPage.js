6057b41b4dc9397424d087d6ac9526f255bde726
ahhzaky <siahmadzaky@gmail.com>
2020-09-21 21:53:46 +0700

    searchPage get object from useGoogleSearch.js

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./SearchPage.css";
 import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term);
 
+  console.log("this is>>", data);
   return (
     <div className="searchPage">
       <div className="searchPage__header">
---
+++ src/keys.js
@@ -0,0 +1,5 @@
+export const API_KEY = "AIzaSyC1B_xz95FYZShd-9QD2qA314qwaZZoezA";
+
+export default API_KEY;
+
+//AIzaSyC1B_xz95FYZShd-9QD2qA314qwaZZoezA
