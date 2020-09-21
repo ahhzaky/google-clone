f66f7651d5160c0524150b477a9f6065f9a90e22
ahhzaky <siahmadzaky@gmail.com>
2020-09-21 21:04:51 +0700

    searchPage call term

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./SearchPage.css";
+import { useStateValue } from "../StateProvider";
 
 function SearchPage() {
-  return <div className="searchPage"></div>;
+  const [{ term }, dispatch] = useStateValue();
+
+  return (
+    <div className="searchPage">
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+      </div>
+      <div className="searchPage__results"></div>
+    </div>
+  );
 }
 
 export default SearchPage;
